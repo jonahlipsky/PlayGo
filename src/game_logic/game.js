62f1d0eaa20643585e032be00b5348b99a3ff7ad
jonahlipsky <jonahlipsky@gmail.com>
2019-02-09 15:36:12 -0500

    About to deploy

--- src/game_logic/game.js
+++ src/game_logic/game.js
@@ -1,7 +1,56 @@
+import firebase from "firebase";
+import { submitBoardPosition, applyNewBoardPosition, loadBoardPosition } from '../utilities/firebase_utils';
+import Board from './board';
 
+export function Game(){
+  const board = new Board(19);
+  board.render();
 
-class Game{
-  constructor(){
+  let form = document.getElementById('game-selection-form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let gameName = document.getElementById('game-name-input');
+    if(gameName.value.length){
+      initializeGame(gameName.value, board);
+      let title = document.getElementById('title');
+      title.innerHTML = gameName.value;
+      form.classList.add('hidden');
+      title.classList.remove('hidden');
+    }
+  });
+}
 
-  }
+function initializeGame(nameOfGame, board){
+
+  var config = {
+    apiKey: "AIzaSyB2XrtPKjoI9FEWJ5I4pWiFVAjV1j8S4AE",
+    authDomain: "js-go-game.firebaseapp.com",
+    databaseURL: "https://js-go-game.firebaseio.com",
+    projectId: "js-go-game",
+    storageBucket: "js-go-game.appspot.com",
+    messagingSenderId: "949423767166"
+  };
+  
+  firebase.initializeApp(config);
+  const db = firebase.firestore();
+
+  let gameName = nameOfGame;
+ 
+  db.collection('games').doc(`${gameName}`).collection('boards')
+    .orderBy('timestamp', 'desc')
+    .limit(1).get().then( snapshot => {
+      snapshot.docChanges().forEach( change => {
+        applyNewBoardPosition.call(this, change.doc.data().gameBoard, board.grid);
+      });
+    });
+
+  loadBoardPosition.call(this, board, gameName);
+  const gameElement = document.getElementById("game-element");
+
+  gameElement.addEventListener('click', function(e){
+    const madeMove = board.moveEvent(e);
+    if(madeMove){
+      submitBoardPosition.call(this, board, gameName);
+    }
+  });
 }
---
+++ src/utilities/firebase_utils.js
@@ -0,0 +1,69 @@
+import firebase from "firebase";
+
+export function submitBoardPosition(board, gameName){
+  // Add a new message entry to the Firebase database.
+  let grid = board.grid;
+  let newGrid = Object.assign({});
+  let keys = Object.keys(grid);
+  for (let x = 0; x < keys.length; x++) {
+    for (let y = 0; y < keys.length; y++) {
+      newGrid[x] = newGrid[x] || {};
+      newGrid[x][y] = newGrid[x][y] || {};
+      newGrid[x][y] = grid[x][y].stone ? grid[x][y].stone.color : null;
+    }
+  }
+  board.previousBoardKoCheck = newGrid;
+  let object = {
+    gameBoard: newGrid,
+    currentTurnColor: board.color,
+    timestamp: firebase.firestore.FieldValue.serverTimestamp()
+  };
+  return firebase.firestore().collection('games').doc(`${gameName}`).collection('boards').add(object)
+    .catch(function(error) {
+    console.error('Error writing new grid to database', error);
+  });
+};
+
+export function loadBoardPosition(board, gameName){
+  const query = firebase.firestore()
+                    .collection('games').doc(`${gameName}`).collection('boards')
+                    .orderBy('timestamp', 'desc')
+                    .limit(1);
+  query.onSnapshot(function(snapshot){
+    // let docChanges = snapshot.docChanges()[0].doc.data();
+    // let previousBoardKoCheck = snapshot.docChanges()[1].doc.data();
+    //for some reason when I set it to limit(2), sometimes it returns one. Is that because
+    //there is only one 'change'?
+    //I think that this could certainly be it... In other words, it fires, the query, 
+    //but then it gets only the 'changes', which would only ever be one board.
+
+    snapshot.docChanges().forEach(function(change){
+      let data = change.doc.data();
+      console.log(`New color received: ${data.currentTurnColor}`);
+      restoreBoardPosition(data.gameBoard, board, data.currentTurnColor);
+    });
+  });                    
+}
+
+export function applyNewBoardPosition(newBoard, oldBoard){
+  for (let x = 0; x < oldBoard.nCrosses; x++) {
+    for (let y = 0; y < oldBoard.nCrosses; y++) {
+      let oldNode = oldBoard.grid[x][y];
+      let newStoneValue = newBoard[x][y];
+      if(!(oldNode.stone === newStoneValue)) { // this means they are not both null
+        if(oldNode.stone && !newStoneValue){ // previously was a stone, now its not
+          oldNode.stone = null;
+          oldNode.updateSelf();
+        } else if (!oldNode.stone && newStoneValue){ // previously wasn't a stone, now it is
+          oldNode.assignStone(newStoneValue);
+        }
+      }
+      oldBoard.render();
+    }
+  }
+}
+
+export function restoreBoardPosition(newBoard, oldBoard, color){
+  oldBoard.color = color;
+  applyNewBoardPosition(newBoard, oldBoard);
+}
